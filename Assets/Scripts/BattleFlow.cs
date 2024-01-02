742212f251477458690b7b7fe292545cb1b73efb
FT-TS03227\Admin <hunganh7793@gmail.com>
2024-01-02 14:48:43 +0700

    no message

--- Assets/Scripts/BattleFlow.cs
+++ Assets/Scripts/BattleFlow.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BattleFlow : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject gameOverUI;
+    public PlayerHealth playerHealth;
+    public GameObject bgMusic;
+
+    public GameObject gameWinUI;
+
     void Start()
     {
-        
+        gameOverUI.SetActive(false);
+        gameWinUI.SetActive(false);
+        playerHealth.onDead += OnGameOver;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(EnemyHealth.LivingEnemeCount <= 0)
+        {
+            OnGameWin();
+        }
     }
+
+    private void OnGameWin()
+    {
+        gameWinUI.SetActive(true);
+        bgMusic.SetActive(false);
+        playerHealth.gameObject.SetActive(false);
+    }
+
+    private void OnGameOver()
+    {
+        gameOverUI.SetActive(true);
+        bgMusic.SetActive(false);
+    }
+
+    public void ReturnToMainMenu() => SceneManager.LoadScene("MainMenu");
 }
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 
 public class EnemyHealth : Health
 {
-    
-
-    
+    public static int LivingEnemeCount;
+    private void Awake() => LivingEnemeCount++;
 
     private void Die()
     {
+        LivingEnemeCount--;
         base.Die();
-        Debug.Log("Enemy Die");
     }
 }
